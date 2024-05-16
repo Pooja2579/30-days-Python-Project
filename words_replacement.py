5b7a5935f98fb9df0216638ce2a32ca76f05521e
Pooja <103728019+Pooja2579@users.noreply.github.com>
2024-05-16 23:14:30 +0530

    Add files via upload

---
+++ words_replacement.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str = "hellloooo I'm coding here"
+    word_to_replace = input('Enter the word:')
+    word_for_replacement = input('Enter the word for replace: ')
+    print(str.replace( word_to_replace, word_for_replacement))
+
+replace_word()
